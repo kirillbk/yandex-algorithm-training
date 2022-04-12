c618ec9f6362e3a9f04cc78c243e92051ef98400
Kirill <kirillbk@gmail.com>
2022-04-12 22:22:01 +0300

    update 2B 4 c

--- 2B/4/c.py
+++ 2B/4/c.py
@@ -2,7 +2,6 @@ from collections import Counter
 from sys import stdin
 
 word_freq = Counter(stdin.read().split())
-answer = [(freq, word) for word, freq in word_freq.items()]
-answer.sort(key=lambda x:(-x[0], x[1]))
+answer = sorted(word_freq.items(), key=lambda x:(-x[1], x[0]))
 
-print('\n'.join(word for _, word in answer))
+print('\n'.join(word for word, _ in answer))
