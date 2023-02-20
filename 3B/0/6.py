c8e81be93f1c095035b8900e99dd43a32011f0a4
Kirill <kirillbk@gmail.com>
2023-02-21 02:12:31 +0300

    3B 0 update 6

--- 3B/0/6.py
+++ 3B/0/6.py
@@ -7,8 +7,8 @@ systems = set()
 for i in range(n):
 	a, b = map(int, input().split())
 	systems.add(i)
-	for p_a, p_b, n in partitions:
-		if a <= p_a <= b or a <= p_b <= b or p_a <= a <= p_b or p_a <= b <= p_b:
-			systems.discard(n)
+	for p_a, p_b, p_i in partitions:
+		if a <= p_b and b >= p_a:
+			systems.discard(p_i)
 	partitions.append((a, b, i))
 print(len(systems))
