ec4255ee6fb5f09c604857ff3770e209e74df0ce
Kirill <kirillbk@gmail.com>
2023-03-18 17:13:30 +0300

    3 final 41

---
+++ 3B/finish/a41.py
@@ -0,0 +1,24 @@
+# A. Подземная доставка
+
+from collections import defaultdict
+
+n = int(input())
+train = list()
+items = defaultdict(int)
+for _ in range(n):
+	cmd = input().split()
+	if cmd[0] == 'add':
+		qty = int(cmd[1])
+		train.append([cmd[2], qty])
+		items[cmd[2]] += qty
+	elif cmd[0] == 'delete':
+		qty = int(cmd[1])
+		while train and qty >= train[-1][1]:
+			qty -= train[-1][1]
+			items[train[-1][0]] -= train[-1][1]
+			train.pop()
+		if train:
+			items[train[-1][0]] -= qty
+			train[-1][1] -= qty
+	else:
+		print(items[cmd[1]])
