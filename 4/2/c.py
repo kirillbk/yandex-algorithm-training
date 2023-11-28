b45d54ace73d129ead77f0503d4e9dec0b34ea93
Kirill <kirillbk@gmail.com>
2023-11-28 05:28:40 +0300

    4 2 *C

--- 4/2/c.py
+++ 4/2/c.py
@@ -14,5 +14,38 @@ def z(s: str) -> list[int]:
 
     return z
 
+
+# поиск значения Z-функции с помощью бинпоиска и хэшей для строки (как в разборе)
+def z_hash(s: str) -> list[int]:
+    n = len(s)
+
+    P = 10**9 + 7
+    X = 257
+
+    x = [0] * (n + 1)
+    x[0] = 1
+    h = [0] * (n + 1)
+    for i in range(1, n + 1):
+        h[i] = (h[i - 1] * X + ord(s[i - 1])) % P
+        x[i] = (x[i - 1] * X) % P
+
+    z = [0] * n
+    for i in range(1, n):
+        l = 0
+        r = n - i + 1
+        while r - l > 1:
+            m = (r + l) // 2
+            if (
+                (h[m] + h[i] * x[m]) % P
+                == (h[i + m] + h[0] * x[m]) % P
+            ):
+                l = m
+            else:
+                r = m
+        z[i] = l
+
+    return z
+
+
 s = input()
-print(*z(s))
+print(*z_hash(s))
